7b563b6b0012bdedc8eb332deba5edbb28009284
mhabib1234 <habibullah.howlader@bjitacademy.com>
2023-07-16 10:49:05 +0600

    updated app.js routing
    updated app.js routing for trainee and admin routes
    
    refs#30058

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
-
+import AdminLayout from './layouts/AdminLayout'
+import TraineeLayout from './layouts/TraineeLayout';
 import Signin from './components/Signin';
+import AdminRoute from './routes/AuteRoutes/AdminRoute';
+import TraineeRoute from './routes/AuteRoutes/TraineeRoute';
 
 // Lazy-loaded components
 const Starter = lazy(() => import('./views/Starter'));
@@ -14,6 +17,18 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/signin" element={<Signin />} />
+          
+          <Route element={<AdminRoute />}>
+            <Route path="admin" element={<AdminLayout />}>
+              <Route path="starter" element={<Starter />} />
+            </Route>
+          </Route>
+
+          <Route element={<TraineeRoute />}>
+            <Route path="trainee" element={<TraineeLayout />}>
+              <Route path="alerts" element={<Alerts />} />
+            </Route>
+          </Route>
 
           <Route path="/" element={<Navigate to="/signin" />} />
         </Routes>
